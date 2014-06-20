04dd1394b5962524062261d6159a9840e19514bf
Mathieu Fenniak <biziqe@mathieu.fenniak.net>
2014-06-20 17:59:21 -0600

    Streaming tweets to the browser successfully from one application

---
+++ run_prod_realtimeserver.py
@@ -0,0 +1,16 @@
+from tornado import ioloop
+from wall import app, streaming_search, shutdown_event
+from threading import Thread
+import os
+
+port = int(os.environ.get("PORT", 5000))
+bind_addr = os.environ.get("BIND_ADDR", "127.0.0.1")
+app.listen(port, address=bind_addr, xheaders=True)
+
+search_thread = Thread(target=streaming_search)
+search_thread.start()
+try:
+    ioloop.IOLoop.instance().start()
+finally:
+    shutdown_event.set()
+    search_thread.join()
---
+++ wall.py
@@ -0,0 +1,98 @@
+from birdy.twitter import StreamClient, TwitterRateLimitError
+from config import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
+from flask import Flask, render_template
+from sockjs.tornado import SockJSRouter, SockJSConnection
+from threading import Event
+from tornado.ioloop import IOLoop
+from tornado.web import FallbackHandler, Application
+from tornado.wsgi import WSGIContainer
+import json
+import time
+
+### SockJS section...
+
+open_connections = []
+
+class SearchConnection(SockJSConnection):
+    def __init__(self, *args, **kwargs):
+        super(SearchConnection, self).__init__(*args, **kwargs)
+        self.ip = None
+
+    def on_open(self, info):
+        open_connections.append(self)
+        self.ip = info.ip
+        print "OPEN   : SockJS from %s" % self.ip
+
+    def on_message(self, msg):
+        print "MSG    : %s" % (msg)
+        msg = json.loads(msg)
+        self.send(json.dumps(msg))
+
+    def on_close(self):
+        open_connections.remove(self)
+        print "CLOSE  : SockJS from %s" % self.ip
+
+
+### Flask UI section...
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+### Twitter streaming search thread...
+
+search_targets = ["#worldcup", "#fra", "#sui"]
+shutdown_event = Event()
+
+def streaming_search():
+    global search_targets
+    search_target = ",".join(search_targets)
+    client = StreamClient(CONSUMER_KEY,
+                        CONSUMER_SECRET,
+                        ACCESS_TOKEN,
+                        ACCESS_TOKEN_SECRET)
+    while not shutdown_event.is_set():
+        try:
+            resource = client.stream.statuses.filter.post(track=search_target)
+            for status in resource.stream():
+                if shutdown_event.is_set():
+                    break
+                if status.get("lang") != "en":
+                    # Ignore non-English tweets
+                    continue
+                if status.get("retweeted_status") != None:
+                    # Ignore retweets
+                    continue
+
+                IOLoop.instance().add_callback(received_tweet, [status])
+
+                print "%s (@%s) -- %s" % (
+                    status.get("user", {}).get("name", "?"),
+                    status.get("user", {}).get("screen_name", "?"),
+                    status.get("text")
+                )
+
+        except TwitterRateLimitError:
+            print "TwitterRateLimitError -- sleeping for five minutes"
+            time.sleep(302)
+            continue
+        #except:
+        #    print "Unknown and unexpected error in streaming search -- sleeping for five minutes"
+        #    time.sleep(303)
+        #    continue
+
+def received_tweet(status):
+    status = json.dumps(status)
+    for connection in open_connections:
+        connection.send(status)
+
+
+### Combine the SockJS & Flask components...
+SearchRouter = SockJSRouter(SearchConnection, '/search', user_settings = { "sockjs_url": "http://cdn.sockjs.org/sockjs-0.3.min.js" })
+urls = SearchRouter.urls
+flask_wrapper = WSGIContainer(app)
+urls.append((r".*", FallbackHandler, dict(fallback=flask_wrapper)))
+app = Application(urls)
