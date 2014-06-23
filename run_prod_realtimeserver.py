effd6b1ffbd1744050f0ecdd85b06712af75d81a
Mathieu Fenniak <biziqe@mathieu.fenniak.net>
2014-06-23 15:55:12 -0600

    Dynamic reconfiguration w/ a reconfigure web page

--- run_prod_realtimeserver.py
+++ run_prod_realtimeserver.py
@@ -1,17 +1,14 @@
-from tornado import ioloop
-from wall import app, streaming_search, shutdown_event, stats_callback
-from threading import Thread
+from tornado.ioloop import IOLoop
+from wall import app, stats_callback, start_streaming_search, terminate_streaming_search
 import os
 
 port = int(os.environ.get("PORT", 5000))
 bind_addr = os.environ.get("BIND_ADDR", "127.0.0.1")
 app.listen(port, address=bind_addr, xheaders=True)
 
-search_thread = Thread(target=streaming_search)
-search_thread.start()
+start_streaming_search()
 try:
     stats_callback.start()
-    ioloop.IOLoop.instance().start()
+    IOLoop.instance().start()
 finally:
-    shutdown_event.set()
-    search_thread.join()
+    terminate_streaming_search()
--- wall.py
+++ wall.py
@@ -1,16 +1,17 @@
 from birdy.twitter import StreamClient, TwitterRateLimitError
 from config import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request, redirect, url_for
+from functools import partial
+from multiprocessing import Pipe, Process
 from sockjs.tornado import SockJSRouter, SockJSConnection
-from threading import Event, Lock
-from tornado.ioloop import IOLoop, PeriodicCallback
+from tornado.ioloop import PeriodicCallback, IOLoop
 from tornado.web import FallbackHandler, Application
 from tornado.wsgi import WSGIContainer
-import json
-import time
-import os
 import datetime
 import HTMLParser
+import json
+import os
+import time
 
 # TODO
 #  * add time since tweet into HTML output
@@ -65,38 +66,79 @@ def favicon():
     return send_from_directory(os.path.join(flask_app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
+@flask_app.route("/reconfigure", methods=['GET', 'POST'])
+def reconfigure():
+    global search_targets, filter_level, english_only
+    if request.method == "POST":
+        terminate_streaming_search()
+        search_targets = [t.strip() for t in request.form.get("searchTerms", "").split(",")]
+        filter_level = request.form.get("filterLevel", "none")
+        english_only = request.form.get("languageEnglish", "off") == "on"
+        start_streaming_search()
+        return redirect(url_for('index'))
+    else:
+        return render_template("reconfigure.html",
+            search_targets=search_targets,
+            english_only=english_only,
+            filter_level=filter_level
+        )
+
+
 ### Twitter streaming search thread...
 
 # FIXME: Support filter_level parameter (none, low, medium, high) to filter tweets server-side
 # FIXME: Support language parameter (eg. "en") to filter tweets server-side
-search_targets = ["#yyc"]
-shutdown_event = Event()
+search_targets = ["World Cup"]
+filter_level = "none"
+english_only = True
 
 # FIXME: A percentage value might be better, and tuneable on the settings page?
 min_time_between_tweets = 5  # minimum number of seconds between tweets
-last_tweet = 0
 
-stats_lock = Lock()
 stats_time = datetime.datetime.utcnow()
 tweets_per_minute = 0
 displayed_tweets_per_minute = 0
 
-def streaming_search():
-    global search_targets, last_tweet, tweets_per_minute, displayed_tweets_per_minute
+# Streaming search process state
+subprocess_to_main1 = subprocess_to_main2 = None
+search_subprocess = None
+
+def start_streaming_search():
+    global subprocess_to_main1, subprocess_to_main2, search_subprocess
+    subprocess_to_main1, subprocess_to_main2 = Pipe(duplex=False)
+    search_subprocess = Process(target=streaming_search, kwargs={
+        "subprocess_to_main": subprocess_to_main2,
+        "search_targets": search_targets
+    })
+    search_subprocess.start()
+    IOLoop.instance().add_handler(
+        subprocess_to_main1.fileno(),
+        partial(on_streaming_search_msg, subprocess_to_main1),
+        IOLoop.READ)
+
+def terminate_streaming_search():
+    IOLoop.instance().remove_handler(subprocess_to_main1.fileno())
+    search_subprocess.terminate()
+    subprocess_to_main1.close()
+    subprocess_to_main2.close()
+
+class MyStreamClient(StreamClient):
+    @staticmethod
+    def get_json_object_hook(data):
+        return data
+
+def streaming_search(subprocess_to_main, search_targets):
+    last_tweet = 0
     search_target = ",".join(search_targets)
-    client = StreamClient(CONSUMER_KEY,
+    client = MyStreamClient(CONSUMER_KEY,
                         CONSUMER_SECRET,
                         ACCESS_TOKEN,
                         ACCESS_TOKEN_SECRET)
-    while not shutdown_event.is_set():
+    while True:
         try:
             resource = client.stream.statuses.filter.post(track=search_target)
             for status in resource.stream():
-                if shutdown_event.is_set():
-                    break
-
-                with stats_lock:
-                    tweets_per_minute += 1
+                subprocess_to_main.send({"action": "tweet"})
 
                 if status.get("lang") != "en":
                     # Ignore non-English tweets
@@ -110,11 +152,8 @@ def streaming_search():
                     continue
                 last_tweet = t
 
-                with stats_lock:
-                    displayed_tweets_per_minute += 1
-
                 status = process_status(status)
-                IOLoop.instance().add_callback(received_tweet, status)
+                subprocess_to_main.send({"action": "displayed_tweet", "tweet": status})
 
         except TwitterRateLimitError:
             print "TwitterRateLimitError -- sleeping for five minutes"
@@ -125,21 +164,27 @@ def streaming_search():
             time.sleep(5)
             continue
 
-def received_tweet(status):
-    msg = {"action": "tweet", "tweet": status}
-    msg = json.dumps(msg)
-    for connection in open_connections:
-        connection.send(msg)
+def on_streaming_search_msg(subprocess_to_main, *args, **kwargs):
+    global tweets_per_minute, displayed_tweets_per_minute
+    obj = subprocess_to_main.recv()
+    if obj.get("action") == "tweet":
+        tweets_per_minute += 1
+    elif obj.get("action") == "displayed_tweet":
+        displayed_tweets_per_minute += 1
+        msg = {"action": "tweet", "tweet": obj.get("tweet")}
+        msg = json.dumps(msg)
+        for connection in open_connections:
+            connection.send(msg)
 
 def transmit_stats():
     global tweets_per_minute, displayed_tweets_per_minute, stats_time
-    with stats_lock:
-        tpm = tweets_per_minute
-        dtpm = displayed_tweets_per_minute
-        last_time = stats_time
-        tweets_per_minute = 0
-        displayed_tweets_per_minute = 0
-        new_time = stats_time = datetime.datetime.utcnow()
+
+    tpm = tweets_per_minute
+    dtpm = displayed_tweets_per_minute
+    last_time = stats_time
+    tweets_per_minute = 0
+    displayed_tweets_per_minute = 0
+    new_time = stats_time = datetime.datetime.utcnow()
 
     time_delta = (new_time - last_time)
     tpm = 60 * tpm / time_delta.total_seconds()
